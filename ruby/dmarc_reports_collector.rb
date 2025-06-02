895e9614cc50d10b2f5733c8af29a0cb97160220
TomoProg <t.yamamoto@neogenia.co.jp>
2025-06-02 15:04:43 +0900

    feat: 初回実行時に認証用のURLを表示し、認証できるように変更した

--- ruby/dmarc_reports_collector.rb
+++ ruby/dmarc_reports_collector.rb
@@ -1,15 +1,38 @@
 require 'googleauth'
+require 'googleauth/stores/file_token_store'
 require 'google/apis/gmail_v1'
 require 'fileutils'
 require 'date'
 
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+APPLICATION_NAME = 'DMARC Report Fetcher'
+CREDENTIALS_PATH = 'credentials.json'
+TOKEN_PATH = 'token.yaml'
+SCOPE = Google::Apis::GmailV1::AUTH_GMAIL_READONLY
+
+def authorize
+  client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
+  token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
+  authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
+  user_id = 'default'
+  credentials = authorizer.get_credentials(user_id)
+  if credentials.nil?
+    url = authorizer.get_authorization_url(base_url: OOB_URI)
+    puts "Open the following URL in the browser and enter the resulting code after authorization:\n" + url
+    print "Enter code: "
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI
+    )
+  end
+  credentials
+end
+
 # Gmail APIのセットアップ
 Gmail = Google::Apis::GmailV1
 gmail_service = Gmail::GmailService.new
-gmail_service.client_options.application_name = 'DMARC Report Fetcher'
-gmail_service.authorization = Google::Auth.get_application_default(
-  ['https://www.googleapis.com/auth/gmail.readonly']
-)
+gmail_service.client_options.application_name = APPLICATION_NAME
+gmail_service.authorization = authorize
 
 # 引数で日付範囲を受け取る (フォーマット: YYYY-MM-DD)
 start_date = ARGV[0] ? Date.parse(ARGV[0]) : Date.today - 7
